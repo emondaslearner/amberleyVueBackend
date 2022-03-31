914b2e9b4382b5783f04789f733a3fd181632816
Emon <emonwordpress.1000@gmaill.com>
2022-03-31 19:51:11 +0600

    push

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const connectDB = require("./connectDB");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
 const fs = require("fs-extra");
-const path = require("path")
+const path = require("path");
 
 const app = express();
 const router = express.Router();
@@ -17,31 +17,37 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-const invoiceModel = mongoose.model("invoice_informations", {
-  name: String,
-  email: String,
-  invoiceNo: Number,
-  purchaseNo: Number,
-  customerName: String,
-  issueDate: Date,
-  dueDate: Date,
-  item: Array,
-  total: Number,
-  description: String,
-  pdf: {pdf:Buffer,contentType:String},
-},{ timestamps: true });
+const invoiceSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    invoiceNo: Number,
+    purchaseNo: Number,
+    customerName: String,
+    issueDate: Date,
+    dueDate: Date,
+    item: Array,
+    total: Number,
+    description: String,
+    pdf: { pdf: Buffer, contentType: String },
+  },
+  { timestamps: true }
+);
 
-router.get('/',(req,res) => {
-  res.json({
-    "it":"lets"
+const Invoice = mongoose.model("invoice_informations", invoiceSchema);
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Babe I am waiting for you!!"
   })
-})
+});
 
 router.post("/doSomething", async (req, res) => {
   try {
     if (req.files == null) {
       const item = JSON.parse(req.body.items);
-      const data = await invoiceModel({
+      const data = await Invoice.create({
         name: req.body.name,
         email: req.body.email,
         invoiceNo: req.body.invoiceNo,
@@ -49,25 +55,24 @@ router.post("/doSomething", async (req, res) => {
         customerName: req.body.customerName,
         issueDate: req.body.issueDate,
         dueDate: req.body.dueDate,
-        item:item,
+        item: item,
         total: req.body.total,
         description: req.body.description,
       });
-      const dataSave = await data.save();
-      res.send(dataSave);
+      res.send(data);
     } else {
       const item = JSON.parse(req.body.items);
       const file = req.files.file;
-      const filePath = `${__dirname}/files/${file.name}`
-      file.mv(filePath, (err) => {
+      const filePath = `${__dirname}/files/${file.name}`;
+      file.mv(filePath, async (err) => {
         if (err) {
           res.send(err);
         }
-        
-      const pdf = {
-        pdf:file.data,
-        contentType:file.mimetype
-      }
+
+        const pdf = {
+          pdf: file.data,
+          contentType: file.mimetype,
+        };
         const invoiceDataWithFile = {
           name: req.body.name,
           email: req.body.email,
@@ -76,16 +81,13 @@ router.post("/doSomething", async (req, res) => {
           customerName: req.body.customerName,
           issueDate: req.body.issueDate,
           dueDate: req.body.dueDate,
-          item:item,
+          item: item,
           total: req.body.total,
           description: req.body.description,
           pdf,
         };
-        const data = invoiceModel(invoiceDataWithFile);
-
-        //save data to database
-        const dataSave =  data.save();
-        res.send(JSON.stringify({name:"success"}))
+        const data = await Invoice.create(invoiceDataWithFile);
+        res.send(data);
       });
     }
   } catch (err) {
